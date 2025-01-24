eafa54b7e234912a6ed0bf245c1058427d9cbcf9
chethan <chethanchethu99339@gmail.com>
2025-01-24 23:48:18 +0530

    update App component to use Gilroy font for the title and adjust font weight

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function App() {
     return (
         <>
-            <div className="font-family">
-                <h1 className="font-bold">Welcome to My Portfolio</h1>
+            <div className="font-[Gilroy]">
+                <h1 className="font-[500]">Welcome to My Portfolio</h1>
                 <p className="font-[400]">
                     This is a paragraph with Poppins font.
                 </p>
