98e60b6caa843f99bdbb2568b8e208517fa12001
chethan <chethanchethu99339@gmail.com>
2025-01-24 23:36:02 +0530

    add Tailwind CSS script and update App component with portfolio content

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import './App.css'
+import React from "react";
 
 function App() {
-  
-
-  return (
-    <>
-      <p className="">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <div className="font-family">
+                <h1 className="font-bold">Welcome to My Portfolio</h1>
+                <p className="font-[400]">
+                    This is a paragraph with Poppins font.
+                </p>
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
