c91e6dc109d53d91d2476e5808bb9305177ad03f
chethan <chethanchethu99339@gmail.com>
2025-01-26 19:16:53 +0530

    refactor HeroContent component; integrate HeadingPara, HeroCard, and Rights components for enhanced layout and functionality

--- src/components/Design/HeroContent.jsx
+++ src/components/Design/HeroContent.jsx
@@ -1,65 +1,38 @@
 import React from "react";
-import logo1 from "../../../public/asstes/icons/Identity.png";
-import logo2 from "../../../public/asstes/icons/CircleWavyCheck.png";
 
 import "../../styles/index.css";
 import GradientButton from "./Content/GradientButton";
+import HeadingPara from "./Content/HeadingPara";
+import HeroCard from "./Content/HeroCard";
+import Rights from "./Content/Rights";
 
-export default function HeroConetent() {
+export default function HeroContent() {
     return (
         <>
             <div
-                className="w-[45vw] p-4 my-10 rounded-4xl translate-y-20 select-none "
+                className="w-[45vw] p-4 my-10 rounded-4xl translate-y-20 select-none"
                 id="hero-content"
             >
                 <div className="mt-20 ml-14">
                     <div>
+                        <HeadingPara
+                            heading="About Us"
+                            hMain={false}
+                            para="There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden." 
+                            pMain={false}
+                        />
                         <div>
-                            <h1 className="font-gilroy-semibold text-7xl leading-20 ">
-                                Your partner increating and growing brands.
-                            </h1>
-                        </div>
-                        <div>
-                            <p className="font-gilroy-regula text-3xl mt-8">
-                                We transform your ideas into reality
-                            </p>
-                        </div>
-                        <div>
-                            <GradientButton />
+                            <GradientButton 
+                            txt={'Get in Touch'} 
+                            GIT={false}
+                            button={true}
+                            />
                         </div>
                     </div>
                 </div>
-                <div className="mt-35">
-                    <div className="flex justify-evenly ">
-                        <div className=" ">
-                            <img src={logo1} alt="" className="w-[3rem]" />
-                            <h4 className="font-poppins font-light text-2xl mt-4">
-                                Strong logontity
-                            </h4>
-                            <p className="font-poppins font-extralight w-[15vw] mt-2 text-[0.94rem] ">
-                                Grow you exposure with a stunning brand identity
-                            </p>
-                        </div>
-                        <div className=" ">
-                            <img src={logo2} alt="" className="w-[3rem]" />
-                            <h4 className="font-poppins font-light text-2xl mt-4">
-                                Strong Identity
-                            </h4>
-                            <p className=" font-poppins font-extralight w-[15vw] mt-2 text-[0.94rem] ">
-                                Grow you exposure with a stunning brand identity
-                            </p>
-                        </div>
-                    </div>
-                    <div></div>
-                </div>
-                <div className="">
-                    <p className="font-gilroy-light translate-y-40 translate-x-15 text-2xl ">
-                        © All rights reserved by{" "}
-                        <span className="font-gilroy font-normal underline">
-                            Divine Innovative
-                        </span>
-                    </p>
-                </div>
+                <HeroCard requied={false} />
+                <Rights rMain={false} />
+
             </div>
         </>
     );
