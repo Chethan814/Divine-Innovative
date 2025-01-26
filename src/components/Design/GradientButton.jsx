9624de2cff01a5ff026d02829dc7b51742a45342
chethan <chethanchethu99339@gmail.com>
2025-01-26 16:08:25 +0530

    refactor HeroContent and GradientButton components; add logos and improve styling

--- src/components/Design/GradientButton.jsx
+++ src/components/Design/GradientButton.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import "../../styles/index.css"
+
 const GradientButton = () => {
   const buttonStyle = {
     background: 'linear-gradient(to right, #F8DCE0 0%, #F5DDE7 12%, #EFDEEF 24%, #ECDFF5 36%, #E6E0FE 48%, #E9DFFF 58%, #ECDCFD 69%, #EFDACF 80%, #F3D8FA 90%, #F5D6FA 100%)',
@@ -10,7 +12,7 @@ const GradientButton = () => {
   };
 
   return (
-    <button className='text-black font-poppins-semibold text-[22px] mt-8' style={buttonStyle}>
+    <button className='font-poppins font-normal text-[22px] mt-8 w-[13vw] h-[3.4rem] transition hover:scale-105 ' style={buttonStyle}>
       Get in Touch
     </button>
   );
--- src/components/Design/HeroContent.jsx
+++ src/components/Design/HeroContent.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import GradientButton from "./GradientButton";
+import logo1 from "../../../public/asstes/icons/Identity.png"
+import logo2 from "../../../public/asstes/icons/CircleWavyCheck.png"
+
+import "../../styles/index.css"
 
 export default function HeroConetent() {
     return (
@@ -10,18 +14,44 @@ export default function HeroConetent() {
             >
                 <div className="mt-20 ml-14">
                     <div>
-                        <h1 className="font-gilroy-semibold text-7xl leading-20">
-                            Your partner increating and growing brands.
-                        </h1>
+                        <div>
+                            <h1 className="font-gilroy-semibold text-7xl leading-20">
+                                Your partner increating and growing brands.
+                            </h1>
+                        </div>
+                        <div>
+                            <p className="font-gilroy-regula text-3xl mt-8">
+                                We transform your ideas into reality
+                            </p>
+                        </div>
+                        <div>
+                            <GradientButton />
+                        </div>
                     </div>
-                    <div>
-                        <p className="font-gilroy-regula text-3xl mt-6">
-                            We transform your ideas into reality
-                        </p>
+                </div>
+                <div className="mt-35">
+                    <div className="flex justify-evenly ">
+                        <div className=" ">
+                            <img src={logo1} alt="" className="w-[3rem]" />
+                            <h4 className="font-poppins font-light text-2xl mt-4">Strong logontity</h4>
+                            <p className="font-poppins font-extralight w-[15vw] mt-2 text-[0.94rem] ">
+                                Grow you exposure with a stunning brand identity
+                            </p>
+                        </div>
+                        <div className=" ">
+                            <img src={logo2} alt="" className="w-[3rem]"/>
+                            <h4 className="font-poppins font-light text-2xl mt-4">Strong Identity</h4>
+                            <p className=" font-poppins font-extralight w-[15vw] mt-2 text-[0.94rem] ">
+                                Grow you exposure with a stunning brand identity
+                            </p>
+                        </div>
                     </div>
-                    <div>
-                        <GradientButton />
-                    </div>
+                    <div></div>
+                </div>
+                <div className="">
+                    <p className="font-gilroy-light translate-y-40 translate-x-15 text-2xl ">
+                    © All rights reserved by <span className="font-gilroy font-normal underline">Divine Innovative</span>
+                    </p>
                 </div>
             </div>
         </>
