e5262d6545e20077cad554a664ef21a8319256a4
chethan <chethanchethu99339@gmail.com>
2025-01-26 22:48:21 +0530

    add Home page with HeroSection component for dynamic content display

---
+++ src/pages/Home.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import HeroSection from "../components/Hero/Hero";
+import "../styles/index.css";
+
+function Home() {
+    return (
+        <>
+            {/* <div className="font-[Gilroy]">
+                <h1 className="font-[500]">Welcome to My Portfolio</h1>
+                <p className="font-[400]">
+                    This is a paragraph with Poppins font.
+                </p>
+            </div> */}
+            <HeroSection
+                main={true}
+                button={true}
+                buttonGIT={true}
+                h1={`Your partner increating and growing brands.`}
+                p={`We transform your ideas into reality`}
+            />
+        </>
+    );
+}
+
+export default Home;
