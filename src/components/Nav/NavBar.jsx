7959b2be67bb0727709dbe4c8709aaadd9659a13
chethan <chethanchethu99339@gmail.com>
2025-01-26 10:17:25 +0530

    add NavBar component with basic structure and styling

---
+++ src/components/Nav/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function NavBar(params) {
+    return (
+        <>
+            <div
+                className="bg-green-300 w-[50vw] h-[10vh] absolute left-10 top-10 "
+                id="nav-bar"
+            >
+                <div>
+                    <img src="" alt="" />
+                </div>
+                <div></div>
+            </div>
+        </>
+    );
+}
