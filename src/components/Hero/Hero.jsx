f8e307a9cd954b7e4ef9695b6c88bc03d7def72e
chethan <chethanchethu99339@gmail.com>
2025-01-26 22:46:14 +0530

    refactor HeroSection component to accept props for dynamic content rendering

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import NavBar from "../Nav/NavBar";
-import HeroConetent from "../Design/HeroContent";
+import HeroContent from "../Design/HeroContent";
 import Minimalistic from "../Design/Minimalistic";
 
-
-export default function HeroSection(){
+export default function HeroSection(props) {
     return (
         <>
-        <div className="w-[95vw] mx-auto bg-[#ffffff] flex  justify-center select-none">
-               <NavBar/>
-               <HeroConetent />
-               <Minimalistic />
+            <div className="w-[95vw] mx-auto bg-[#ffffff] flex  justify-center select-none ">
+                <NavBar />
+                <HeroContent
+                    main={props.main}
+                    button={props.button}
+                    buttonGIT={props.buttonGIT}
+                    h1={props.h1}
+                    p={props.p}
+                />
+                <Minimalistic />
             </div>
         </>
-    )
+    );
 }
