e11860fddbe5e894d46b10273a47b2ab0f2615e0
chethan <chethanchethu99339@gmail.com>
2025-01-29 23:17:32 +0530

    style: remove unused react.svg asset; update HeroSection component for improved layout and responsiveness

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,15 +4,11 @@ import HeroContent from "../Design/HeroContent";
 import Minimalistic from "../Design/Minimalistic";
 
 export default function HeroSection(props) {
-    const gradientStyle = {
-        background:
-            "linear-gradient(90deg, #F5D6FA 0%, #F3D8FB 21%, #E9DFFF 47%, #F5DDE7 73%, #F8DCE0 100%)",
-    };
     return (
         <>
             <div
-                className="bg-linear-[90deg,#F5D6FA_0%,#F3D8FB_21%,#E9DFFF_47%,#F5DDE7_73%,#F8DCE0_100%]  sm:-bg-linear-0 sm:bg-red-400 md:bg-blue-300 lg:bg-green-300 xl:bg-purple-300 2xl:bg-yellow-300 || 
-            || w-[100vw] xl:w-[95vw] 2xl:w-[90vw] || xl:mx-auto || flex  justify-center select-none " 
+                className="bg-linear-[90deg,#F5D6FA_0%,#F3D8FB_21%,#E9DFFF_47%,#F5DDE7_73%,#F8DCE0_100%]  sm:-bg-linear-0 sm:bg-red-400 md:bg-blue-300 lg:bg-green-300 xl:bg-purple-300 2xl:bg-yellow-300  
+             w-[100vw] h-[100%] xl:w-[95vw] 2xl:w-[90vw]  xl:mx-auto  flex  justify-center select-none "
             >
                 <div className=" xl:flex flex-col xl:mt-10 xl:ml-5 ">
                     <NavBar />
