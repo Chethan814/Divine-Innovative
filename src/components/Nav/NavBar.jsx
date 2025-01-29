bf794a65c292854c3aecb3bae42dde3b1d1752cf
chethan <chethanchethu99339@gmail.com>
2025-01-29 08:02:57 +0530

    style: improve NavBar layout with responsive adjustments and enhanced structure

--- src/components/Nav/NavBar.jsx
+++ src/components/Nav/NavBar.jsx
@@ -6,22 +6,22 @@ export default function NavBar() {
     return (
         <>
             <div
-                className="w-[50vw] h-[10vh] absolute left-10 top-10 || flex items-center justify-between"
+                className=" w-[98vw] xl:w-[50vw] 2xl:w-[50rem] xl:h-[10vh] mt-8 mx-auto xl:ml-8 xl:pl-2 flex items-center justify-around 2xl:justify-around "
                 id="nav-bar"
             >
-                <div className="flex items-center gap-4 ">
-                    <NavLink to="/" className="flex items-center gap-4">
+                <div>
+                    <NavLink to="/" className="flex flex-row items-center xl:gap-4 ">
                         <div>
-                            <img src={Logo} alt="Logo" className="w-18" />
+                            <img src={Logo} alt="Logo" className="w-10 xl:w-18" />
                         </div>
-                        <div className="font-gilroy-semibold text-[1.6rem] -translate-y-[0.1rem]">
-                            <span className="text-[2rem] ">D</span>ivine{" "}
-                            <span className="text-[1.8rem]">I</span>nnovative
+                        <div className="hidden sm:block font-gilroy-semibold text-[1rem] xl:text-[1.6rem] xl:-translate-y-[0.1rem]">
+                            <span className="text-[1.3rem] xl:text-[2rem] ">D</span>ivine{" "}
+                            <span className="text-[1.3rem] xl:text-[1.8rem]">I</span>nnovative
                         </div>
                     </NavLink>
                 </div>
                 <div>
-                    <ul className="flex items-center justify-between gap-16 pr-8 translate-y-[0.1rem] || font-gilroy-light text-[1.35rem] ">
+                    <ul className="  w-57 flex items-center justify-between xl:gap-16 xl:2xl:gap- xl:pr-8 xl:translate-y-[0.1rem] || font-gilroy-light xl:text-[1.35rem] ">
                         <li>
                             <NavLink to="/aboutus ">About us</NavLink>{" "}
                         </li>
@@ -32,8 +32,8 @@ export default function NavBar() {
                             <NavLink to="/Portfolio">Portfolio</NavLink>
                         </li>
                     </ul>
-                    <hr className=" text-black rotate-90 w-9 translate-x-23 -translate-y-3 rounded-2xl" />
-                    <hr className=" text-black rotate-90 w-9 translate-x-58 -translate-y-3 rounded-2xl" />
+                    <hr className="hidden xl:block text-black rotate-90 w-9 translate-x-23 -translate-y-3 rounded-2xl" />
+                    <hr className="hidden xl:block text-black rotate-90 w-9 translate-x-58 -translate-y-3 rounded-2xl" />
                 </div>
             </div>
         </>
