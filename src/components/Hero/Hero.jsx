1759f799a907f4520ea2c83bf8b7bfa930269ba5
chethan <chethanchethu99339@gmail.com>
2025-01-29 08:02:42 +0530

    style: enhance HeroSection layout with responsive background colors and improved structure

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,15 +6,20 @@ import Minimalistic from "../Design/Minimalistic";
 export default function HeroSection(props) {
     return (
         <>
-            <div className="w-[95vw] mx-auto bg-[#ffffff] flex  justify-center select-none ">
-                <NavBar />
-                <HeroContent
-                    main={props.main}
-                    button={props.button}
-                    buttonGIT={props.buttonGIT}
-                    h1={props.h1}
-                    p={props.p}
-                />
+            <div
+                className=" bg-[#ffffff] sm:bg-red-300 md:bg-blue-300 lg:bg-green-300 xl:bg-purple-300 2xl:bg-yellow-300 || 
+            || w-[100vw] xl:w-[95vw] 2xl:w-[90vw] || xl:mx-auto || flex  justify-center select-none "
+            >
+                <div className=" xl:flex flex-col xl:mt-10 xl:ml-5 ">
+                    <NavBar />
+                    <HeroContent
+                        main={props.main}
+                        button={props.button}
+                        buttonGIT={props.buttonGIT}
+                        h1={props.h1}
+                        p={props.p}
+                    />
+                </div>
                 <Minimalistic />
             </div>
         </>
