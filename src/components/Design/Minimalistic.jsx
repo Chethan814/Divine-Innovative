bbd391ec391e2f5b988c176f33f67a505dcc3ea8
chethan <chethanchethu99339@gmail.com>
2025-01-31 21:46:06 +0530

    style: enhance layout and responsiveness of Minimalistic and Hero components for better visual alignment on various screen sizes

--- src/components/Design/Minimalistic.jsx
+++ src/components/Design/Minimalistic.jsx
@@ -14,7 +14,7 @@ import icon8 from "/asstes/hero-section/shaps/svg/Ornament8.svg";
 export default function Minimalistic(){
     return (
         <>
-        <div className="hidden sm:block  sm:w-[45vw] xl:w-[43vw] 2xl:w-[47rem] sm:h-[80vh] xl:h-[150vh] 2xl:h-[72rem] mx-auto  xl:mx-8 sm:my-10 rounded-4xl xl:translate-x-4   " id="Minimalistic-design">
+        <div className="hidden sm:block  sm:w-[45vw] xl:w-[43vw] 2xl:w-[47rem] sm:h-[80vh] xl:h-[150vh] 2xl:h-[72rem] mx-auto  xl:mx-8 sm:my-10 rounded-4xl sm:-translate-x-3 xl:translate-x-4   " id="Minimalistic-design">
                     <div className="">
                         <div className="absolute">
                             <img
@@ -23,14 +23,14 @@ export default function Minimalistic(){
                                 className="sm:w-[45vw]  2xl:w-[47rem] sm:h-[80vh] xl:h-[30rem] 2xl:h-[72rem]  rounded-4xl px-[0.1rem] object-cover "
                             />
                             <div className="">
-                                <img src={icon1} alt="" className=" absolute bottom-35 sm:-translate-x-12  xl:-translate-x-14 2xl:-translate-x-13 sm:translate-y-26 sm:w-26 2xl:w-66" />
-                                <img src={icon2} alt="" className=" absolute sm:top-14 xl:top-50 right-0 sm:translate-x-3 xl:translate-x-3 2xl:translate-x-14  xl:w-68 sm:w-33 2xl:w-85" />
-                                <img src={icon3} alt="" className=" absolute sm:top-30 xl:top-60 2xl:top-90 sm:-translate-x-8 xl:-translate-x-19 sm:w-30 2xl:w-70" />
-                                <img src={icon4} alt="" className=" absolute sm:bottom-16 xl:bottom-18 sm:left-28 xl:left-41 2xl:left-47 sm:w-26 2xl:w-50" />
+                                <img src={icon1} alt="" className=" absolute bottom-35 sm:-translate-x-12  xl:-translate-x-14 2xl:-translate-x-13 sm:translate-y-13 sm:w-38 2xl:w-66" />
+                                <img src={icon2} alt="" className=" absolute sm:top-20 xl:top-50 right-0 sm:translate-x-6 xl:translate-x-3 2xl:translate-x-14 sm:w-40 xl:w-68  2xl:w-85" />
+                                <img src={icon3} alt="" className=" absolute sm:top-30 xl:top-60 2xl:top-90 sm:-translate-x-11 xl:-translate-x-19 sm:w-31 2xl:w-70" />
+                                <img src={icon4} alt="" className=" absolute sm:bottom-13 xl:bottom-18 sm:left-15 xl:left-41 2xl:left-47 sm:w-19 2xl:w-50" />
                                 <img src={icon5} alt="" className=" absolute sm:top-10 xl:top-22 sm:left-22 xl:left-32 2xl:left-36 sm:w-16 2xl:w-40" />
-                                <img src={icon6} alt="" className=" absolute sm:bottom-40 xl:bottom-99 sm:-right-0 xl:right-0 translate-x-2 2xl:translate-x-4 sm:w-20 2xl:w-80 " />
-                                <img src={icon7} alt="" className=" absolute sm:bottom-43 xl:bottom-72 sm:-right-0 xl:right-0 2xl:translate-y-5 2xl:translate-x-4 sm:w-25 2xl:w-80" />
-                                <img src={icon8} alt="" className=" absolute sm:bottom-53 xl:bottom-67 sm:-right-0 xl:right-0 translate-x-2 2xl:translate-y-5 2xl:translate-x-4 sm:w-20 2xl:w-52" />
+                                <img src={icon6} alt="" className=" absolute sm:bottom-53 xl:bottom-99 sm:-right-0 xl:right-0 translate-x-2 2xl:translate-x-4 sm:w-33 2xl:w-80 " />
+                                <img src={icon7} alt="" className=" absolute sm:bottom-41 xl:bottom-72 sm:-right-0 xl:right-0 2xl:translate-y-5 2xl:translate-x-4 sm:w-31 2xl:w-80" />
+                                <img src={icon8} alt="" className=" absolute sm:bottom-37 xl:bottom-67 sm:-right-0 xl:right-0 translate-x-2 2xl:translate-y-5 2xl:translate-x-4 sm:w-26 2xl:w-52" />
                             </div>
                         </div>
 
