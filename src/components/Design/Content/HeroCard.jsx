9d091db071bddaec00c9861d88d57296fbeb7bc5
chethan <chethanchethu99339@gmail.com>
2025-02-01 07:36:03 +0530

    style: enhance layout and responsiveness of HeroCard component for improved visual alignment and spacing

--- src/components/Design/Content/HeroCard.jsx
+++ src/components/Design/Content/HeroCard.jsx
@@ -6,12 +6,12 @@ import logo2 from "/asstes/icons/CircleWavyCheck.png";
 const HeroCarditems = ({ logo, title, description }) => {
     return (
         <>
-            <div className="mt-10 border rounded-2xl flex flex-col items-center text-center w-70 p-2 ">
-                <img src={logo} alt="" className="w-[3rem]" />
-                <h4 className="font-poppins font-light text-2xl mt-4">
+            <div className="mt-10 border lg:border-0 rounded-2xl flex flex-col items-center lg:items-start text-center lg:text-left w-70 lg:w-62 p-2 lg:p-0 lg:mx-0  ">
+                <img src={logo} alt="" className="w-[3rem] lg:w-10" />
+                <h4 className="font-poppins font-light text-2xl lg:text-[1.3rem] mt-4 ">
                     {title}
                 </h4>
-                <p className="font-poppins font-extralight xl:w-[15vw] mt-2 text-[0.94rem]">
+                <p className="font-poppins font-extralight lg:w-60 xl:w-[15vw] mt-2 text-[0.94rem] lg:text-[0.8rem]">
                     {description}
                 </p>
             </div>
@@ -26,7 +26,7 @@ export default function HeroCard(props) {
     }
     return (
         <>
-            <div className="xl:mt-35 block sm:hidden lg:block">
+            <div className=" lg:ml-16 xl:mt-35 block sm:hidden lg:block">
                 <div className="flex flex-col md:flex-row justify-evenly">
                     <HeroCarditems
                         logo={logo1}
@@ -36,7 +36,7 @@ export default function HeroCard(props) {
                     <HeroCarditems
                         logo={logo2}
                         title={`Award Winning`}
-                        description={`We create the unconventionalproducts and eye catching.`}
+                        description={`We create the unconventional products and eye catching.`}
                     />
                 </div>
             </div>
