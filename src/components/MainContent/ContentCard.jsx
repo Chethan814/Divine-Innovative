78ce8d101e99d0daa97ba8b3ca28b675191cb6ea
chethan <chethanchethu99339@gmail.com>
2025-02-01 23:40:03 +0530

    feat: enhance ContentCard component with dynamic list rendering and improved layout

--- src/components/MainContent/ContentCard.jsx
+++ src/components/MainContent/ContentCard.jsx
@@ -1,13 +1,43 @@
 import React from "react";
+import "../../styles/index.css";
+import GradientButton from "../Design/Content/GradientButton";
 
-export default function ContentCard() {
+function List({ listValues, Contents }) {
+    if (!Contents) {
+        return (
+            <ul className="list-disc ml-4 sm:ml-5 mt-5 sm:mt-6 sm:text-[1rem]">
+                {listValues.map((value, index) => (
+                    <li key={index} className="my-2">
+                        {value}
+                    </li>
+                ))}
+            </ul>
+        );
+    } else {
+        return <></>;
+    }
+}
+
+// sm:bg-red-400 md:bg-blue-400 lg:bg-green-400 xl:bg-pink-400 2xl:bg-amber-400
+
+function ContentCard({ index, heading, description, Contents, listValue }) {
     return (
-        <>
-            <div>Services</div>
-            <div>Heading</div>
-            <div>Description</div>
-            <div>Points</div>
-            <div>button</div>
-        </>
+        <section className="w-[80vw] sm:w-[90vw] xl:w-[80vw] mx-auto my-20 p-2 ">
+            <div className="text-[0.8rem] pb-1">Services 0{index}</div>
+            <div className="font-gilroy text-4xl sm:text-5xl">
+                <h2>{heading}</h2>
+            </div>
+            <div className="text-[0.9rem] sm:text-[1rem] text-left w-70 sm:w-75 md:w-[20rem] my-1 sm:mt-8">
+                <p>{description}</p>
+            </div>
+            <div>
+                <List Contents={Contents} listValues={listValue} />
+            </div>
+            <div className="lg:-translate-x-6 xl:translate-x-0">
+                <GradientButton button={true} GIT={true} />
+            </div>
+        </section>
     );
 }
+
+export default ContentCard;
