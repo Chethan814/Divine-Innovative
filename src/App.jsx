ff480c69b88aff5b0511d7415621d81d5046e379
chethan <chethanchethu99339@gmail.com>
2025-02-01 19:50:39 +0530

    feat: add Portfolio route and update Services routes in App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./styles/index.css";
 
 // Pages
-import Home from './pages/Home.jsx'
-import Aboutus from './pages/Aboutus.jsx'
-import Services from "./pages/Services/Services.jsx"
-import Design from "./pages/Services/Design.jsx"
-import Development from "./pages/Services/Development.jsx"
-import Management from "./pages/Services/Management.jsx"
+import Home from "./pages/Home.jsx";
+import Aboutus from "./pages/Aboutus.jsx";
+import Services from "./pages/Services/Services.jsx";
+import Design from "./pages/Services/Design/Design.jsx";
+import Development from "./pages/Services/Development/Development.jsx";
+import Management from "./pages/Services/Management/Management.jsx";
+import Portfolio from "./pages/Portfolio/Portfolio.jsx";
 
 function App() {
     return (
@@ -17,9 +18,13 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/aboutus" element={<Aboutus />} />
+                    <Route path="/services" element={<Services />} />
+                        <Route path="/Design" element={<Design />} />
+                        <Route path="/Portfolio" element={<Portfolio />} />
+                        <Route path="/Development" element={<Development />} />
+                    
                 </Routes>
             </Router>
-            
         </>
     );
 }
