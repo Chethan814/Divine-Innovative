db9d71ece36783781528e6376681cb57ad78f793
chethan <chethanchethu99339@gmail.com>
2025-02-01 23:40:21 +0530

    feat: implement dynamic content rendering in Aboutus page with multiple ContentCard components

--- src/pages/Aboutus.jsx
+++ src/pages/Aboutus.jsx
@@ -5,14 +5,51 @@ import "../styles/index.css";
 import ContentCard from "../components/MainContent/ContentCard";
 
 function Aboutus() {
+    const content = {
+        Design: {
+            Services: 1,
+            Heading: `WEB DESIGN`,
+            Description: `A best-in-class mixture of technology and art in our projects captivate you with elegance and style of visuals, contents and interactions in our solutions. The insights provided by the creative vision, paired with the modern technology touch, render a completely satisfying user experience in every design solution we offer.`,
+            List: {
+                Contents: true,
+                li: [
+                    "Web Interface",
+                    "User Experience",
+                    "Design System",
+                    "Wireframe",
+                    "Prototype",
+                ],
+            },
+        },
+
+        VISUAL: {
+            Services: 2,
+            Heading: `VISUAL IDENTITY.`,
+            Description: `A best-in-class mixture of technology and art in our projects captivate you with elegance and style of visuals, contents and interactions in our solutions. The insights provided by the creative vision, paired with the modern technology touch, render a completely satisfying user experience in every design solution we offer.`,
+            List: {
+                Contents: true,
+                li: [
+                    "Logo Designing",
+                    "Social media",
+                    "Marketing Assets",
+                    "Presentations",
+                ],
+            },
+        },
+
+        BANNER: {
+            Services: 3,
+            Heading: `BANNER DESIGN`,
+            Description: `A best-in-class mixture of technology and art in our projects captivate you with elegance and style of visuals, contents and interactions in our solutions. The insights provided by the creative vision, paired with the modern technology touch, render a completely satisfying user experience in every design solution we offer.`,
+            List: {
+                Contents: true,
+                li: ["Story board", "Hand Sketch", "Moodboard", "Digital art"],
+            },
+        },
+    };
+
     return (
         <>
-            {/* <div className="font-[Gilroy]">
-                <h1 className="font-[500]">Welcome to My Portfolio</h1>
-                <p className="font-[400]">
-                    This is a paragraph with Poppins font.
-                </p>
-            </div> */}
             <HeroSection
                 main={false}
                 button={true}
@@ -24,8 +61,27 @@ function Aboutus() {
                     If you are going to use a passage of Lorem Ipsum, 
                     you need to be sure there isn't anything embarrassing hidden.`}
             />
-            <ContentCard />
-            
+            <ContentCard
+                index={content.Design.Services}
+                heading={content.Design.Heading}
+                description={content.Design.Description}
+                list={content.Design.List.Contents}
+                listValue={content.Design.List.li}
+            />
+            <ContentCard
+                index={content.VISUAL.Services}
+                heading={content.VISUAL.Heading}
+                description={content.VISUAL.Description}
+                list={content.VISUAL.List.Contents}
+                listValue={content.VISUAL.List.li}
+            />
+            <ContentCard
+                index={content.BANNER.Services}
+                heading={content.BANNER.Heading}
+                description={content.BANNER.Description}
+                list={content.BANNER.List.Contents}
+                listValue={content.BANNER.List.li}
+            />
         </>
     );
 }
