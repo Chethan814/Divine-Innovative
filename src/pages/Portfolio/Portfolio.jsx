5c3fe07f1651ef4235a545a998326f2342fa0ec3
chethan <chethanchethu99339@gmail.com>
2025-02-01 19:45:43 +0530

    feat: add Portfolio page with HeroSection component for showcasing projects

---
+++ src/pages/Portfolio/Portfolio.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import HeroSection from "../../components/Hero/Hero";
+import "../../styles/index.css";
+import { faL, fas } from "@fortawesome/free-solid-svg-icons";
+
+function Portfolio() {
+    return (
+        <>
+            <HeroSection
+                main={false}
+                button={true}
+                buttonGIT={false}
+                h1={`PORTFOLIO`}
+                p={`A best-in-class mixture of technology and art in our projects captivate you with elegance and style of visuals, 
+                    contents and interactions in our solutions. 
+                    The insights provided by the creative vision, 
+                    paired with the modern technology touch, 
+                    render a completely satisfying user experience in every Portfolio solution we offer.`}
+            />
+        </>
+    );
+}
+
+export default Portfolio;
