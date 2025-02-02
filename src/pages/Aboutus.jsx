ba61060f1d0cf24f94e208dd089b6a1104958c23
chethan <chethanchethu99339@gmail.com>
2025-02-02 22:55:04 +0530

    feat: refactor Aboutus page to use renderContentCard function for dynamic ContentCard rendering

--- src/pages/Aboutus.jsx
+++ src/pages/Aboutus.jsx
@@ -5,10 +5,25 @@ import "../styles/index.css";
 import ContentCard from "../components/MainContent/ContentCard";
 import Footer from "../components/Footer/Footer";
 
+const renderContentCard = (content) => (
+    <ContentCard
+        Tagline={content.Services.Tagline}
+        indexNeeded={content.Services.indexNeeded}
+        index={content.Services.index}
+        heading={content.Heading}
+        description={content.Description}
+        list={content.List.Contents}
+        listValue={content.List.li}
+    />
+);
 function Aboutus() {
     const content = {
         Design: {
-            Services: 1,
+            Services: {
+                Tagline: "Service",
+                indexNeeded: true,
+                index: "1",
+            },
             Heading: `WEB DESIGN`,
             Description: `A best-in-class mixture of technology and art in our projects captivate you with elegance and style of visuals, contents and interactions in our solutions. The insights provided by the creative vision, paired with the modern technology touch, render a completely satisfying user experience in every design solution we offer.`,
             List: {
@@ -22,9 +37,12 @@ function Aboutus() {
                 ],
             },
         },
-
         VISUAL: {
-            Services: 2,
+            Services: {
+                Tagline: "Service",
+                indexNeeded: true,
+                index: "1",
+            },
             Heading: `VISUAL IDENTITY.`,
             Description: `A best-in-class mixture of technology and art in our projects captivate you with elegance and style of visuals, contents and interactions in our solutions. The insights provided by the creative vision, paired with the modern technology touch, render a completely satisfying user experience in every design solution we offer.`,
             List: {
@@ -37,9 +55,12 @@ function Aboutus() {
                 ],
             },
         },
-
         BANNER: {
-            Services: 3,
+            Services: {
+                Tagline: "Service",
+                indexNeeded: true,
+                index: "1",
+            },
             Heading: `BANNER DESIGN`,
             Description: `A best-in-class mixture of technology and art in our projects captivate you with elegance and style of visuals, contents and interactions in our solutions. The insights provided by the creative vision, paired with the modern technology touch, render a completely satisfying user experience in every design solution we offer.`,
             List: {
@@ -62,28 +83,10 @@ function Aboutus() {
                     If you are going to use a passage of Lorem Ipsum, 
                     you need to be sure there isn't anything embarrassing hidden.`}
             />
-            <ContentCard
-                index={content.Design.Services}
-                heading={content.Design.Heading}
-                description={content.Design.Description}
-                list={content.Design.List.Contents}
-                listValue={content.Design.List.li}
-            />
-            <ContentCard
-                index={content.VISUAL.Services}
-                heading={content.VISUAL.Heading}
-                description={content.VISUAL.Description}
-                list={content.VISUAL.List.Contents}
-                listValue={content.VISUAL.List.li}
-            />
-            <ContentCard
-                index={content.BANNER.Services}
-                heading={content.BANNER.Heading}
-                description={content.BANNER.Description}
-                list={content.BANNER.List.Contents}
-                listValue={content.BANNER.List.li}
-            />
-            <Footer/>
+            {renderContentCard(content.Design)}
+            {renderContentCard(content.VISUAL)}
+            {renderContentCard(content.BANNER)}
+            <Footer />
         </>
     );
 }
