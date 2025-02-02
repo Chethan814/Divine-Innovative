5949b6d6f06ae097044d249b0870d0cb8d6c840b
chethan <chethanchethu99339@gmail.com>
2025-02-02 15:46:27 +0530

    feat: add GitInTouch component for user contact form

---
+++ src/components/Design/GitInTouch.jsx
@@ -0,0 +1,35 @@
+// import here
+
+// to here
+
+function GitInTouch() {
+    return (
+        <section>
+            <div>
+                <h2>main headin</h2>
+                <p>Descrition</p>
+            </div>
+            <div>
+                <form action="">
+                    <div>
+                        <label htmlFor="Name">name</label>
+                        <input type="text" />
+                    </div>
+                    <div>
+                        <label htmlFor="email">email</label>
+                        <input type="text" />
+                    </div>
+                    <div>
+                        <label htmlFor="Message">Message</label>
+                        <textarea name="message" id=""></textarea>
+                    </div>
+                    <div>
+                        <button >send now</button>
+                    </div>
+                </form>
+            </div>
+        </section>
+    );
+}
+
+export default GitInTouch;
