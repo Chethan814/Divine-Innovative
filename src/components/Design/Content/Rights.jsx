1168f4188d9f1814cc26f71a48331c6ba3ed36bb
chethan <chethanchethu99339@gmail.com>
2025-02-12 07:28:45 +0530

    refactor: update NavLink in GradientButton for correct path and clean up Rights component by removing commented code

--- src/components/Design/Content/Rights.jsx
+++ src/components/Design/Content/Rights.jsx
@@ -17,14 +17,16 @@ const Rights = ({ rMain }) => {
                         </span>
                     </div>
                 ) : (
-                    <div>
-                        <p className="font-gilroy-light xl:translate-y-10 xl:translate-x-15 my-5 sm:my-0 sm:mt-26 lg:mt-10 xl:mt- text-center md:text-left xl:text-2xl ">
-                            © All rights reserved by 
-                            <span className="font-gilroy font-normal underline">
-                                Divine Innovative
-                            </span>
-                        </p>
-                    </div>
+                    <>
+                        {/* <div>
+                            <p className="font-gilroy-light xl:translate-y-10 xl:translate-x-15 my-5 sm:my-0 sm:mt-26 lg:mt-10 xl:mt- text-center md:text-left xl:text-2xl ">
+                                © All rights reserved by
+                                <span className="font-gilroy font-normal underline">
+                                    Divine Innovative
+                                </span>
+                            </p>
+                        </div> */}
+                    </>
                 )}
             </div>
         </>
