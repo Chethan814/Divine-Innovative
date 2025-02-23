91db0e7411a6af4d555a7e3c9f88036f71923d75
chethan <chethanchethu99339@gmail.com>
2025-02-23 15:19:41 +0530

    feat: implement layout component and update routing structure for nested routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-// import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./styles/index.css";
+import Layout from "./pages/Layout.jsx";
 
 // Pages
 import Home from "./pages/Home.jsx";
@@ -17,18 +17,33 @@ import Management from "./pages/Services/Management/Management.jsx";
 import Portfolio from "./pages/Portfolio/Portfolio.jsx";
 
 const router = createBrowserRouter([
-    { path: "/", element: <Home /> },
-    { path: "/aboutus", element: <Aboutus /> },
-    { path: "/portfolio", element: <Portfolio /> },
-    { path: "/services", element: <Services /> },
-    { path: "/services/design", element: <Design /> },
-    { path: "/services/design/graphicdesign", element: <GraphicDesign /> },
-    { path: "/services/design/motiondesign", element: <MotionDesign /> },
-    { path: "/services/design/nicheservices", element: <NicheServices /> },
-    { path: "/services/design/printdesign", element: <PrintDesign /> },
-    { path: "/services/design/webdesign", element: <WebDesign /> },
-    { path: "/services/development", element: <Development /> },
-    { path: "/services/management", element: <Management /> },
+    {
+        path: "/",
+        element: <Layout />,
+        children: [
+            { path: "/", element: <Home /> },
+            { path: "/aboutus", element: <Aboutus /> },
+            { path: "/portfolio", element: <Portfolio /> },
+            { path: "/services", element: <Services /> },
+            { path: "/services/design", element: <Design /> },
+            {
+                path: "/services/design/graphicdesign",
+                element: <GraphicDesign />,
+            },
+            {
+                path: "/services/design/motiondesign",
+                element: <MotionDesign />,
+            },
+            {
+                path: "/services/design/nicheservices",
+                element: <NicheServices />,
+            },
+            { path: "/services/design/printdesign", element: <PrintDesign /> },
+            { path: "/services/design/webdesign", element: <WebDesign /> },
+            { path: "/services/development", element: <Development /> },
+            { path: "/services/management", element: <Management /> },
+        ],
+    },
 ]);
 
 function App() {
