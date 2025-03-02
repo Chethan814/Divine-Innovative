fb8911a915e678d2a45ab0c7b0ec12cc9c22b7db
chethan <chethanchethu99339@gmail.com>
2025-03-02 12:29:18 +0530

    feat: implement PhoneNav component for mobile navigation support in NavBar

--- src/components/Nav/NavBar.jsx
+++ src/components/Nav/NavBar.jsx
@@ -1,16 +1,8 @@
-import { useState } from "react";
+import PhoneNav from "./PhoneNav";
 import Logo from "/asstes/Logo/BLACK-logo.png";
 import { NavLink } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 
 export default function NavBar() {
-    const [phoneNav, setPhoneNav] = useState(false);
-
-    function phoneNavHandling() {
-        setPhoneNav((navState) => !navState);
-    }
-
     return (
         <div
             className="w-[85vw] sm:w-[45vw] md:w-[40vw] lg:w-[45vw] xl:w-auto 2xl:w-[50rem] xl:h-[10vh] mt-8 xl:mt-3 mx-auto sm:mx-0 sm:mt-12 lg:px-4 xl:ml-8 xl:pl-2 flex items-center justify-around lg:justify-between 2xl:justify-around"
@@ -34,76 +26,8 @@ export default function NavBar() {
                 </div>
             </NavLink>
             <div>
-                <div className="sm:hidden translate-x-8">
-                    <span className="text-3xl" onClick={phoneNavHandling}>
-                        <FontAwesomeIcon icon={faBars} />
-                    </span>
-                </div>
-                <div
-                    className={`${
-                        phoneNav
-                            ? "bg-white z-100 w-[100vw] overflow-hidden h-[100vh] absolute top-0 left-0 sm:bg-transparent sm:hidden"
-                            : "hidden"
-                    }`}
-                >
-                    <div className="absolute right-8 top-8">
-                        <span className="text-4xl" onClick={phoneNavHandling}>
-                            <FontAwesomeIcon icon={faXmark} />
-                        </span>
-                    </div>
-                    <div className="w-full h-full">
-                        <ul className="w-full h-full flex flex-col items-center justify-center gap-13 text-2xl font-bold">
-                            <li>
-                                <NavLink
-                                    className={({ isActive }) =>
-                                        isActive
-                                            ? "underline underline-offset-4"
-                                            : ""
-                                    }
-                                    to="/"
-                                    end
-                                >
-                                    Home
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink
-                                    className={({ isActive }) =>
-                                        isActive
-                                            ? "underline underline-offset-4"
-                                            : ""
-                                    }
-                                    to="/aboutus"
-                                >
-                                    About us
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink
-                                    className={({ isActive }) =>
-                                        isActive
-                                            ? "underline underline-offset-4"
-                                            : ""
-                                    }
-                                    to="/Services"
-                                >
-                                    Services
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink
-                                    className={({ isActive }) =>
-                                        isActive
-                                            ? "underline underline-offset-5"
-                                            : ""
-                                    }
-                                    to="/Packages"
-                                >
-                                    Packages
-                                </NavLink>
-                            </li>
-                        </ul>
-                    </div>
+                <div>
+                    <PhoneNav />
                 </div>
                 <ul className="hidden sm:w-57 lg:w-auto sm:flex items-center justify-between xl:justify-start xl:items-start lg:gap-13 xl:pr-8 xl:translate-y-[0.1rem] xl:translate-x-15 font-gilroy-light text-[0.999rem] xl:text-[1.35rem]">
                     <li>
---
+++ src/components/Nav/PhoneNav.jsx
@@ -0,0 +1,90 @@
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
+
+function PhoneNav() {
+    const [isNavOpen, setIsNavOpen] = useState(false);
+
+    function handleNavToggle() {
+        setIsNavOpen((prevState) => !prevState);
+    }
+
+    return (
+        <>
+            <div className="sm:hidden translate-x-8">
+                <span className="text-3xl" onClick={handleNavToggle}>
+                    <FontAwesomeIcon icon={faBars} />
+                </span>
+            </div>
+            <div
+                className={`${
+                    isNavOpen
+                        ? "bg-white z-100 w-[100vw] overflow-hidden h-[100vh] absolute top-0 left-0 sm:bg-transparent sm:hidden"
+                        : "hidden"
+                }`}
+            >
+                <div className="absolute right-8 top-8">
+                    <span className="text-4xl" onClick={handleNavToggle}>
+                        <FontAwesomeIcon icon={faXmark} />
+                    </span>
+                </div>
+                <div className="w-full h-full">
+                    <ul className="w-full h-full flex flex-col items-center justify-center gap-13 text-2xl font-bold">
+                        <li>
+                            <NavLink
+                                className={({ isActive }) =>
+                                    isActive
+                                        ? "underline underline-offset-4"
+                                        : ""
+                                }
+                                to="/"
+                                end
+                            >
+                                Home
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink
+                                className={({ isActive }) =>
+                                    isActive
+                                        ? "underline underline-offset-4"
+                                        : ""
+                                }
+                                to="/aboutus"
+                            >
+                                About us
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink
+                                className={({ isActive }) =>
+                                    isActive
+                                        ? "underline underline-offset-4"
+                                        : ""
+                                }
+                                to="/Services"
+                            >
+                                Services
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink
+                                className={({ isActive }) =>
+                                    isActive
+                                        ? "underline underline-offset-5"
+                                        : ""
+                                }
+                                to="/Packages"
+                            >
+                                Packages
+                            </NavLink>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default PhoneNav;
