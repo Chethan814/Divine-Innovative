3328cc277166454a88f35f8355656f590e762484
chethan <chethanchethu99339@gmail.com>
2025-03-07 21:17:32 +0530

    todays update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,14 @@
 
 import HeroSection from "../components/Hero/Hero";
 import "../styles/index.css";
-import FooterRights from "../components/Footer/FooterRights";
+import Info from "../components/Info/Info";
+import Swift from "../asstes/Project/Chethan/swiftrides.png";
+import Footer from "../components/Footer/Footer";
+// import PhoneNav from "../components/Nav/PhoneNav";
 
 function Home() {
+    const Webdesign_description = `We excel at transforming your ideas, thoughts, and vision into custom web designs that showcase your business and turn prospects into clients. Our dedicated in-house team conducts thorough analyses to craft designs that effectively convey your business's identity, mission, and vision.`;
+
     return (
         <>
             {/* <div className="font-[Gilroy]">
@@ -22,10 +27,12 @@ function Home() {
                 link={`info`}
                 card={true}
             />
-            <FooterRights />
+            {/* <PhoneNav /> */}
+            <div className="my-20">
+                <Info image={Swift} description={Webdesign_description} />
+            </div>
             
-            
-            
+            <Footer />
         </>
     );
 }
