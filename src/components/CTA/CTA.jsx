00adb3fc1a5123a33bb99f45ff0a253aeed478df
chethan <chethanchethu99339@gmail.com>
2025-03-09 18:54:38 +0530

    feat: update CTASection content and styling for improved engagement and visual appeal

--- src/components/CTA/CTA.jsx
+++ src/components/CTA/CTA.jsx
@@ -5,15 +5,13 @@ export default function CTASection() {
                 <div className="p-8 md:p-12 lg:px-16 lg:py-24">
                     <div className="mx-auto max-w-lg text-center">
                         <h2 className="text-2xl font-bold text-gray-900 md:text-3xl">
-                            Lorem, ipsum dolor sit amet consectetur adipisicing
-                            elit
+                            Skills to enrich your online business
                         </h2>
 
                         <p className="hidden text-gray-500 sm:mt-4 sm:block">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing
-                            elit. Quae dolor officia blanditiis repellat in,
-                            vero, aperiam porro ipsum laboriosam consequuntur
-                            exercitationem incidunt tempora nisi?
+                            Boost your online business. Expert digital
+                            solutions, tailored for your success. Get started
+                            now.
                         </p>
                     </div>
 
@@ -27,13 +25,14 @@ export default function CTASection() {
                                 <input
                                     type="email"
                                     placeholder="Email address"
-                                    className="w-full rounded-md border-gray-200 bg-white p-3 text-gray-700 shadow-xs transition focus:border-white focus:ring-3 focus:ring-yellow-400 focus:outline-hidden"
+                                    className="w-full rounded-md border-gray-200 bg-white p-3 text-gray-700 shadow-xs transition focus:border-white focus:ring-3  focus:outline-hidden "
+                                    required
                                 />
                             </div>
 
                             <button
                                 type="submit"
-                                className="group mt-4 flex w-full items-center justify-center gap-2 rounded-md bg-rose-600 px-5 py-3 text-white transition focus:ring-3 focus:ring-yellow-400 focus:outline-hidden sm:mt-0 sm:w-auto"
+                                className="group mt-4 flex w-full items-center justify-center gap-2 rounded-md  px-5 py-3 transition  sm:mt-0 sm:w-auto bg-gradient-to-r from-[#F8DCE0] via-[#E6E0FE] to-[#F5D6FA]"
                             >
                                 <span className="text-sm font-medium">
                                     {" "}
