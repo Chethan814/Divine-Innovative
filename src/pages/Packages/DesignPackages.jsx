2bb2fe177a0e379bf73bc69b3930752a732d876b
chethan <chethanchethu99339@gmail.com>
2025-03-09 23:23:42 +0530

    feat: enhance DesignPackages component with tabbed pricing display and new sections

--- src/pages/Packages/DesignPackages.jsx
+++ src/pages/Packages/DesignPackages.jsx
@@ -1,11 +1,48 @@
-function DesignPackages(){
+import { useState } from "react";
+import Footer from "../../components/Footer/Footer";
+import HeroSection from "../../components/Hero/Hero";
+import PricingSection from "../../components/Pricing/Pricing";
+import TabButton from "../../components/TabButton/TabButton";
+import pricingPlans from "../../components/Pricing/PricingData";
+
+function DesignPackages() {
+    const [selectedTab, setSelectedTab] = useState('Graphic');
+
+    function tabClick(selectedTab) {
+        setSelectedTab(selectedTab);
+    }
+
     return (
         <>
+            <HeroSection
+                main={false}
+                button={true}
+                buttonGIT={false}
+                link={`info`}
+                h1={`Design Pricing`}
+                p={`We believe in clear, upfront pricing that aligns with the unique scope of your project. Our approach is collaborative, ensuring you receive a design solution that not only meets your objectives but exceeds your expectations. Contact us for a personalized quote.`}
+            />
 
-        
+            <section>
+                <div>
+                    <TabButton onSelect={() => tabClick("Graphic")}>
+                        Graphic Design
+                    </TabButton>
+                    <TabButton onSelect={() => tabClick("Web")}>
+                        Web Design
+                    </TabButton>
+                    <TabButton onSelect={() => tabClick("SocialMedia")}>
+                        Social Media Design
+                    </TabButton>
+                </div>
+                <div>
+                    <PricingSection Plans={pricingPlans[selectedTab]} />
+                </div>
+            </section>
+
+            <Footer />
         </>
-    )
+    );
 }
-
 
 export default DesignPackages;
