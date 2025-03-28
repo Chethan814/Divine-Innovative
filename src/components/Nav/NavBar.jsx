17dd612918a92fc305188247d112074afe107e10
chethan <chethanchethu99339@gmail.com>
2025-03-28 21:03:02 +0530

    feat: add fade-in animations to NavBar and PhoneNav components for enhanced user experience

--- src/components/Nav/NavBar.jsx
+++ src/components/Nav/NavBar.jsx
@@ -1,6 +1,6 @@
-
 import Logo from "/asstes/Logo/BLACK-logo.png";
 import { NavLink } from "react-router-dom";
+import { Fade } from "react-awesome-reveal";
 
 export default function NavBar() {
     return (
@@ -8,50 +8,58 @@ export default function NavBar() {
             className="w-full sm:w-[45vw] md:w-[40vw] lg:w-[45vw] xl:w-auto 2xl:w-[50rem] xl:h-[10vh] mt-8 xl:mt-3 mx-auto sm:mx-0 sm:mt-12 lg:px-4 xl:ml-8 xl:pl-2 flex items-center justify-around lg:justify-between 2xl:justify-around select-none"
             id="nav-bar"
         >
-            <NavLink to="/" className="">
-                <img
-                    src={Logo}
-                    alt="Logo"
-                    className="w-10 sm:w-13 xl:w-16 -translate-x-13 sm:translate-x-0 lg:translate-x-6 xl:-translate-x-12"
-                />
-                
-            </NavLink>
+            <Fade direction="down" triggerOnce={true}>
+                <NavLink to="/" className="select-none">
+                    <img
+                        src={Logo}
+                        alt="Logo"
+                        className="w-10 sm:w-13 xl:w-16 -translate-x-13 sm:translate-x-0 lg:translate-x-6 xl:-translate-x-12"
+                    />
+                </NavLink>
+            </Fade>
             <div>
-                
-                <ul className="hidden sm:w-57 lg:w-auto sm:flex items-center justify-between xl:justify-start xl:items-start lg:gap-13 xl:pr-8 xl:translate-y-[0.1rem] xl:translate-x-15 font-gilroy-light text-[0.999rem] xl:text-[1.35rem]">
-                    <li>
-                        <NavLink
-                            className={({ isActive }) =>
-                                isActive ? "underline underline-offset-4" : ""
-                            }
-                            to="/aboutus"
-                        >
-                            About us
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink
-                            className={({ isActive }) =>
-                                isActive ? "underline underline-offset-4" : ""
-                            }
-                            to="/Services"
-                        >
-                            Services
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink
-                            className={({ isActive }) =>
-                                isActive ? "underline underline-offset-5" : ""
-                            }
-                            to="/Packages"
-                        >
-                            Packages
-                        </NavLink>
-                    </li>
-                </ul>
-                <hr className="hidden lg:block text-black rotate-90 w-9 translate-x-17 xl:translate-x-37 -translate-y-3 rounded-2xl" />
-                <hr className="hidden lg:block text-black rotate-90 w-9 translate-x-42 xl:translate-x-68 -translate-y-3 rounded-2xl" />
+                <Fade cascade damping={0.4} direction="down" triggerOnce={true} delay={500}>
+                    <ul className="hidden sm:w-57 lg:w-auto sm:flex items-center justify-between xl:justify-start xl:items-start lg:gap-13 xl:pr-8 xl:translate-y-[0.1rem] xl:translate-x-15 font-gilroy-light text-[0.999rem] xl:text-[1.35rem]">
+                        <li>
+                            <NavLink
+                                className={({ isActive }) =>
+                                    isActive
+                                        ? "underline underline-offset-4"
+                                        : ""
+                                }
+                                to="/aboutus"
+                            >
+                                About us
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink
+                                className={({ isActive }) =>
+                                    isActive
+                                        ? "underline underline-offset-4"
+                                        : ""
+                                }
+                                to="/Services"
+                            >
+                                Services
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink
+                                className={({ isActive }) =>
+                                    isActive
+                                        ? "underline underline-offset-5"
+                                        : ""
+                                }
+                                to="/Packages"
+                            >
+                                Packages
+                            </NavLink>
+                        </li>
+                    </ul>
+                    <hr className="hidden lg:block text-black rotate-90 w-9 translate-x-17 xl:translate-x-37 -translate-y-3 rounded-2xl" />
+                    <hr className="hidden lg:block text-black rotate-90 w-9 translate-x-42 xl:translate-x-68 -translate-y-3 rounded-2xl" />
+                </Fade>
             </div>
         </div>
     );
--- src/components/Nav/PhoneNav.jsx
+++ src/components/Nav/PhoneNav.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
-// import NavDropDown from "./NavDropDown";
+import { Fade } from "react-awesome-reveal";
 
 function PhoneNav() {
     const [isNavOpen, setIsNavOpen] = useState(false);
@@ -15,9 +15,11 @@ function PhoneNav() {
         <>
             <div>
                 <div className="sm:hidden z-100 absolute right-11 top-9 ">
-                    <span className="text-3xl" onClick={handleNavToggle}>
-                        <FontAwesomeIcon icon={faBars} />
-                    </span>
+                    <Fade direction="down" triggerOnce={true}>
+                        <span className="text-3xl" onClick={handleNavToggle}>
+                            <FontAwesomeIcon icon={faBars} />
+                        </span>
+                    </Fade>
                 </div>
                 <div
                     className={`${
@@ -32,57 +34,59 @@ function PhoneNav() {
                         </span>
                     </div>
                     <div className="h-[100vw] w-full translate-y-20  ">
-                        <ul className="h-[100vw] w-full flex flex-col items-center justify-center gap-13 text-2xl font-bold">
-                            <li>
-                                <NavLink
-                                    className={({ isActive }) =>
-                                        isActive
-                                            ? "underline underline-offset-4"
-                                            : ""
-                                    }
-                                    to="/"
-                                    end
-                                >
-                                    Home
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink
-                                    className={({ isActive }) =>
-                                        isActive
-                                            ? "underline underline-offset-4"
-                                            : ""
-                                    }
-                                    to="/aboutus"
-                                >
-                                    About us
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink
-                                    className={({ isActive }) =>
-                                        isActive
-                                            ? "underline underline-offset-4"
-                                            : ""
-                                    }
-                                    to="/Services"
-                                >
-                                    Services
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink
-                                    className={({ isActive }) =>
-                                        isActive
-                                            ? "underline underline-offset-5"
-                                            : ""
-                                    }
-                                    to="/Packages"
-                                >
-                                    Packages
-                                </NavLink>
-                            </li>
-                        </ul>
+                        <Fade cascade damping={0.3}>
+                            <ul className="h-[100vw] w-full flex flex-col items-center justify-center gap-13 text-2xl font-bold">
+                                <li>
+                                    <NavLink
+                                        className={({ isActive }) =>
+                                            isActive
+                                                ? "underline underline-offset-4"
+                                                : ""
+                                        }
+                                        to="/"
+                                        end
+                                    >
+                                        Home
+                                    </NavLink>
+                                </li>
+                                <li>
+                                    <NavLink
+                                        className={({ isActive }) =>
+                                            isActive
+                                                ? "underline underline-offset-4"
+                                                : ""
+                                        }
+                                        to="/aboutus"
+                                    >
+                                        About us
+                                    </NavLink>
+                                </li>
+                                <li>
+                                    <NavLink
+                                        className={({ isActive }) =>
+                                            isActive
+                                                ? "underline underline-offset-4"
+                                                : ""
+                                        }
+                                        to="/Services"
+                                    >
+                                        Services
+                                    </NavLink>
+                                </li>
+                                <li>
+                                    <NavLink
+                                        className={({ isActive }) =>
+                                            isActive
+                                                ? "underline underline-offset-5"
+                                                : ""
+                                        }
+                                        to="/Packages"
+                                    >
+                                        Packages
+                                    </NavLink>
+                                </li>
+                            </ul>
+                        </Fade>
                     </div>
                 </div>
             </div>
