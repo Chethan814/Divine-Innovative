f57c2945463b30477172c6c47155493442a348b0
chethan <chethanchethu99339@gmail.com>
2025-03-29 00:34:17 +0530

    feat: add fade-in animations to ContentCard and ReviewCard components for enhanced visual engagement

--- src/components/MainContent/ContentCard.jsx
+++ src/components/MainContent/ContentCard.jsx
@@ -3,6 +3,7 @@
 import React from "react";
 import "../../styles/index.css";
 import GradientButton from "../Design/Content/GradientButton";
+import { Fade } from "react-awesome-reveal";
 
 function List({ listValues, Contents }) {
     return !Contents ? (
@@ -27,25 +28,39 @@ function ContentCard({
     Contents,
     listValue,
     navlinks,
-    Button
+    Button,
 }) {
     return (
         <section className="w-[80vw] sm:w-[90vw] xl:w-[80vw] mx-auto my-20 p-2 ">
-            <div className="text-[0.8rem] pb-1">
-                {Tagline} {indexNeeded ? `0${index}` : ""}
-            </div>
-            <div className="font-gilroy text-4xl sm:text-5xl max-w-[35rem]">
-                <h2>{heading}</h2>
-            </div>
-            <div className="text-[0.9rem] sm:text-[1rem] text-left w-70 sm:w-75 md:w-[20rem] my-1 sm:mt-8">
-                <p>{description}</p>
-            </div>
-            <div>
-                <List Contents={Contents} listValues={listValue} />
-            </div>
-            <div className="lg:-translate-x-6 xl:translate-x-0">
-                <GradientButton button={Button} GIT={false} navlinks={navlinks} />
-            </div>
+            <Fade  delay={600} triggerOnce={true} direction="down">
+                <div className="text-[0.8rem] pb-1">
+                    {Tagline} {indexNeeded ? `0${index}` : ""}
+                </div>
+            </Fade>
+            <Fade  delay={600} triggerOnce={true} direction="up">
+                <div className="font-gilroy text-4xl sm:text-5xl max-w-[35rem]">
+                    <h2>{heading}</h2>
+                </div>
+            </Fade>
+            <Fade  delay={600} triggerOnce={true} direction="up">
+                <div className="text-[0.9rem] sm:text-[1rem] text-left w-70 sm:w-75 md:w-[20rem] my-1 sm:mt-8">
+                    <p>{description}</p>
+                </div>
+            </Fade>
+            <Fade  delay={600} triggerOnce={true} direction="up">
+                <div>
+                    <List Contents={Contents} listValues={listValue} />
+                </div>
+            </Fade>
+            <Fade  delay={600} triggerOnce={true} direction="up">
+                <div className="lg:-translate-x-6 xl:translate-x-0">
+                    <GradientButton
+                        button={Button}
+                        GIT={false}
+                        navlinks={navlinks}
+                    />
+                </div>
+            </Fade>
         </section>
     );
 }
