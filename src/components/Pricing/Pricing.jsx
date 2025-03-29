168a3ea11101f4a814a1af72102785fc31986ec5
chethan <chethanchethu99339@gmail.com>
2025-03-29 08:22:36 +0530

    feat: add fade-in animation to PricingCard for enhanced visual engagement

--- src/components/Pricing/Pricing.jsx
+++ src/components/Pricing/Pricing.jsx
@@ -1,63 +1,74 @@
 /* eslint-disable react/prop-types */
 
-const PricingCard = ({ title, description, price, features }) => {
-  return (
-    <div className="flex flex-col max-w-lg p-6 mx-auto text-center text-gray-900 bg-white border border-gray-100 rounded-lg shadow xl:p-8">
-      <h3 className="mb-4 text-2xl font-semibold">{title}</h3>
-      <p className="font-light sm:text-lg">{description}</p>
-      <div className="flex items-baseline justify-center my-8">
-        <span className="mr-2 text-5xl font-extrabold">${price}</span>
-        <span>/month</span>
-      </div>
-      <ul className="mb-8 space-y-4 text-left">
-        {features.map((feature, index) => (
-          <li key={index} className="flex items-center space-x-3">
-            <svg
-              className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                clipRule="evenodd"
-              />
-            </svg>
-            <span>{feature}</span>
-          </li>
-        ))}
-      </ul>
-      <a
-        href="#"
-        className="text-white bg-purple-600 hover:bg-purple-700 focus:ring-4 focus:ring-purple-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white dark:focus:ring-purple-900"
-      >
-        Get started
-      </a>
-    </div>
-  );
+import { Fade } from "react-awesome-reveal";
+
+const PricingCard = ({ title, description, price, features, index }) => {
+    return (
+        <Fade cascade delay={index * 800} direction="up">
+            <div className="flex flex-col max-w-lg p-6 mx-auto text-center text-gray-900 bg-white border border-gray-100 rounded-lg shadow xl:p-8">
+                <h3 className="mb-4 text-2xl font-semibold">{title}</h3>
+                <p className="font-light sm:text-lg">{description}</p>
+                <div className="flex items-baseline justify-center my-8">
+                    <span className="mr-2 text-5xl font-extrabold">
+                        ${price}
+                    </span>
+                    <span>/month</span>
+                </div>
+                <ul className="mb-8 space-y-4 text-left">
+                    {features.map((feature, index) => (
+                        <li key={index} className="flex items-center space-x-3">
+                            <svg
+                                className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    fillRule="evenodd"
+                                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                                    clipRule="evenodd"
+                                />
+                            </svg>
+                            <span>{feature}</span>
+                        </li>
+                    ))}
+                </ul>
+                <a
+                    href="#"
+                    className="text-white bg-purple-600 hover:bg-purple-700 focus:ring-4 focus:ring-purple-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white dark:focus:ring-purple-900"
+                >
+                    Get started
+                </a>
+            </div>
+        </Fade>
+    );
 };
 
 const PricingSection = ({ Plans }) => {
-  return (
-    <section className="font-gilroy">
-      <div className="max-w-screen-xl px-4 py-8 mx-auto lg:py-24 lg:px-6">
-        <div className="max-w-screen-md mx-auto mb-8 text-center lg:mb-12">
-          <h2 className="mb-4 text-3xl font-extrabold tracking-tight">
-            Designed for business teams like yours
-          </h2>
-          <p className="mb-5 font-light text-gray-500 sm:text-xl dark:text-gray-400">
-            Here at Landwind we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.
-          </p>
-        </div>
-        <div className="space-y-8 sm:space-y-0 sm:grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {Plans.map((plan, index) => (
-            <PricingCard key={index} {...plan} />
-          ))}
-        </div>
-      </div>
-    </section>
-  );
+    return (
+        <section className="font-gilroy">
+            <div className="max-w-screen-xl px-4 py-8 mx-auto lg:py-24 lg:px-6">
+                <div className="max-w-screen-md mx-auto mb-8 text-center lg:mb-12">
+                    <h2 className="mb-4 text-3xl font-extrabold tracking-tight">
+                        Designed for business teams like yours
+                    </h2>
+                    <p className="mb-5 font-light text-gray-500 sm:text-xl dark:text-gray-400">
+                        Here at Landwind we focus on markets where technology,
+                        innovation, and capital can unlock long-term value and
+                        drive economic growth.
+                    </p>
+                </div>
+
+                <div className="space-y-8 sm:space-y-0 sm:grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                    {Plans.map((plan, index) => (
+                        <div key={index}>
+                            <PricingCard {...plan} index={index} />
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default PricingSection;
